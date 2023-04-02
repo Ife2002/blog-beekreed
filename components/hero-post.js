073e24b5145cc79517a2bc734beaeaa22a9e2fa9
Ife Asiadiachi <iasiadiachi@gmail.com>
2023-04-02 23:32:01 +0000

    response

--- components/hero-post.js
+++ components/hero-post.js
@@ -24,22 +24,22 @@ export default function HeroPost({
        {/* <h1 className="text-[2rem] my-12 text-white font-sans font-bold tracking-tighter leading-tight md:pr-8">
       Latest post
       </h1> */}
-      <div className="mb-1  md:p-12 md:flex md:flex-row font-sans border-b-[1px] border-[#3a3a3a] ">
+      <div className="mb-1  md:p-12 lg:flex lg:flex-row font-sans border-b-[1px] border-[#3a3a3a] ">
      
-        <div className=' bg-white w-[100%]'>
+        <div className=' bg-white lg:object-cover w-[100%]'>
         <CoverImage slug={slug} title={title} image={coverImage} priority />
         </div>
-        <div className="w-[100%] flex items-stretch bg-purple-600 p-8">
+        <div className="w-[100%] flex items-stretch lg:items-center bg-purple-600 p-8">
         <div className="text-[#929292] md:grid md: md:gap-x-16 lg:gap-x-8 ">
         <div>
-        <div className='w-[5rem] font-sans sm:text-[1.1rem] text-purple-200 font-medium  rounded-[0.1rem]'>
+        <div className=' flex font-sans xl:text-[1.5rem] text-[1.1rem] text-purple-200 font-medium  rounded-[0.1rem]'>
        <h1 className='bg-purple-500 px-4 py-2'>Article</h1>
         </div>
-          <h3 className="mb-4 mt-5 mr-1 font-sans text-white font-semibold text-[1.3rem] xl:leading-tight lg:text-[1.7rem] ">
+          <h3 className="mb-4 mt-5 xl:mt-8 mr-1 font-sans text-white font-semibold text-[1.3rem] xl:text-[2.5rem] xl:leading-tight lg:text-[1.7rem] ">
               {title}
           </h3>
-          <div className='text-white'>
-          <p className=" lg:mr-9 mr-7 mb-3 text-[#d4d4d4] font-sans font-medium lg:text-[1rem] text-[1.1rem] leading-relaxed">{excerpt}</p>
+          <div className='text-white xl:mt-8'>
+          <p className=" lg:mr-9 mr-7 mb-3 text-[#d4d4d4] font-sans font-medium xl:text-[1.8rem] lg:text-[1.3rem] text-[1.1rem] leading-relaxed">{excerpt}</p>
           {/* <Date dateString={date} /> */}
         </div>
           {/* 
--- components/more-stories.js
+++ components/more-stories.js
@@ -2,11 +2,11 @@ import PostPlug from './post-plug'
 
 export default function MoreStories({ posts }) {
   return (
-    <section className='bg-black px-4 '>
-      <h2 className="mb-8 text-white font-sans font-normal text-[1.4rem] tracking-tighter leading-tight">
+    <section className='bg-black '>
+      <h2 className="mb-8 text-white font-sans font-normal text-[2rem] tracking-tighter leading-tight">
         More Stories
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-1 ">
+      <div className="grid grid-cols-1 md:grid-cols-1 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 ">
         {posts.map((post) => (
           <PostPlug
             key={post.slug}
